908759a9d9c826947dd258f7ccc6cc17b4e97e14
deadseal001 <liwenbo4936@gmail.com>
2023-01-11 01:23:56 -0600

    auth

--- server/server.js
+++ server/server.js
@@ -1,16 +1,17 @@
 const express = require('express');
-//import ApolloServer
-const {ApolloServer}= require('apollo-server-express');
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
 
-// import our typeDefs and resolvers
-const { typeDefs, resolvers} =require ('./schemas');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 const app = express();
@@ -23,15 +24,14 @@ const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   // integrate our Apollo server with the Express application as middleware
   server.applyMiddleware({ app });
-
-
+  
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
-    });
-  });
-};
-
-
-// Call the async function to start the server
-startApolloServer(typeDefs, resolvers);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+  };
+  
+  // Call the async function to start the server
+  startApolloServer(typeDefs, resolvers);
